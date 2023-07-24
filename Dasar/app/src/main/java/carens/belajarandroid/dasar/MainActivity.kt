d27eeb2c98fe74ec64cbc75fb2461ca2ae0f7245
carenswijaya01 <carenskurniawanwijaya@gmail.com>
2023-07-24 13:36:43 +0700

    Add previous course

--- Dasar/app/src/main/java/carens/belajarandroid/dasar/MainActivity.kt
+++ Dasar/app/src/main/java/carens/belajarandroid/dasar/MainActivity.kt
@@ -1,6 +1,10 @@
 package carens.belajarandroid.dasar
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,34 +17,30 @@ import androidx.compose.ui.tooling.preview.Preview
 import carens.belajarandroid.dasar.ui.theme.DasarTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var nameEditText: EditText
+    private lateinit var sayHelloButton: Button
+    private lateinit var sayHelloTextView: TextView
+
+    private fun initComponents(){
+        nameEditText = findViewById(R.id.nameEditText)
+        sayHelloButton = findViewById(R.id.sayHelloButton)
+        sayHelloTextView = findViewById(R.id.sayHelloTextView)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            DasarTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+        setContentView(R.layout.hello_world)
+        initComponents()
+
+        sayHelloButton.setOnClickListener {
+            Log.i("Carens", "Say hello button clicked!")
+            val name = nameEditText.text.toString()
+            sayHelloTextView.text = resources.getString(R.string.sayHelloTextView, name)
+
+            resources.getStringArray(R.array.names).forEach {
+                Log.i("Carens", it)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DasarTheme {
-        Greeting("Android")
     }
 }
